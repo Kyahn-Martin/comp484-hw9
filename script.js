0af00fd1adc9bc092424cc95ae1b16378ef4a3dc
Kyahn-Martin <kyahn.martin.264@my.csun.edu>
2026-04-06 16:16:54 -0700

    Fixed one comment , added comments to sripts.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Makes a new date object to grab the current month, day, an year
+// Makes a new date object to grab the current month, day, and year
 var date = new Date();
 var day = date.getDate();
 var month = date.getMonth() + 1;
@@ -28,13 +28,18 @@ else
     {
 
     }
+// Where Javascript is modifying the html
+// Displaying the date
 document.getElementById('date-display').innerHTML = "<h2> Today is " + month  + "/" + day + "/" + year + " <br></h2>";
+// Showing number conversions
 document.getElementById('conversion-display').innerHTML += "<h2> Original vaue: '" + num1 + "' Converted: '" + Number(num1) + "' isNAN: '" + Number.isNaN(Number(num1)) + "' isInteger: '" + Number.isInteger(Number(num1)) + "'<br></h2>";
 document.getElementById('conversion-display').innerHTML += "<h2> Original vaue: '" + num2 + "' Converted: '" + Number(num2) + "' isNAN: '" + Number.isNaN(Number(num2)) + "' isInteger: '" + Number.isInteger(Number(num2)) + "'<br></h2>";
 document.getElementById('conversion-display').innerHTML += "<h2> Original vaue: '" + word + "' Converted: '" + Number(word) + "' isNAN: '" + Number.isNaN(Number(word)) + "' isInteger: '" + Number.isInteger(Number(word)) + "'<br></h2>";
 document.getElementById('conversion-display').innerHTML += "<h2> Original vaue: '" + float + "' Converted: '" + Number(float) + "' isNAN: '" + Number.isNaN(Number(float)) + "' isInteger: '" + Number.isInteger(Number(float)) + "'<br></h2>";
+// Showing simple math equations
 document.getElementById('math-display').innerHTML += "<h2> 5 + 10 = " + (5 + 10) + "<br></h2>";
 document.getElementById('math-display').innerHTML += "<h2> 5 * 10 = " + (5 * 10) + "<br></h2>";
+// Showing formatted numbers
 document.getElementById('math-display').innerHTML += "<h2> The current us population: " + UsPop.toLocaleString()+ "<br></h2>";
 document.getElementById('math-display').innerHTML += "<h2> The current global population: " + GlobalPop.toLocaleString()+ "<br></h2>";
 document.getElementById('math-display').innerHTML += "<h2> Percentage of the global population that are american: " + (UsPop/GlobalPop).toLocaleString() +"%<br></h2>";
